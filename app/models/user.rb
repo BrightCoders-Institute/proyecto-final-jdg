03db77bc9063841088c92e0198b16fba526a0acb
hugocruz1234 <root@LAPTOP-TC1RTTI2.localdomain>
2023-08-01 23:10:54 -0600

    some validations were added to the registration form and model testing was added

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   self.inheritance_column = :type
+
+  validates :first_name, :last_name, presence: true
   # Devise modules and other common user attributes and methods
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,19 @@
-require "test_helper"
+# test/models/user_test.rb
+require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save user without first_name" do
+    user = User.new(last_name: "Doe", email: "john@example.com", password: "password")
+    assert_not user.save, "Saved the user without a first_name"
+  end
+
+  test "should not save user without last_name" do
+    user = User.new(first_name: "John", email: "john@example.com", password: "password")
+    assert_not user.save, "Saved the user without a last_name"
+  end
+
+  test "should save user with first_name and last_name" do
+    user = User.new(first_name: "John", last_name: "Doe", email: "john@example.com", password: "password")
+    assert user.save, "Failed to save the user with first_name and last_name"
+  end
 end
