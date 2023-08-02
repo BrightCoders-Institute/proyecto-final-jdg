035ff8a265105df24a0f96f6adc94a1d6f3619c1
hugocruz1234 <42016840+hugocruz1234@users.noreply.github.com>
2023-08-02 13:52:12 -0600

    Update test/models/user_test.rb
    Co-authored-by: Juan Aguilar <50384228+JuanequeX@users.noreply.github.com>

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,18 +2,29 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  class UserTest < ActiveSupport::TestCase
+
+  def create_valid_user(first_name, last_name)
+    User.new(first_name: first_name, last_name: last_name, email: "john@example.com", password: "password")
+  end
+
   test "should not save user without first_name" do
-    user = User.new(last_name: "Doe", email: "john@example.com", password: "password")
-    assert_not user.save, "Saved the user without a first_name"
+    user = create_valid_user(nil, "Doe")
+    assert_not user.valid?
+    assert_includes user.errors[:first_name], "can't be blank"
   end
 
   test "should not save user without last_name" do
-    user = User.new(first_name: "John", email: "john@example.com", password: "password")
-    assert_not user.save, "Saved the user without a last_name"
+    user = create_valid_user("John", nil)
+    assert_not user.valid?
+    assert_includes user.errors[:last_name], "can't be blank"
   end
 
   test "should save user with first_name and last_name" do
-    user = User.new(first_name: "John", last_name: "Doe", email: "john@example.com", password: "password")
+    user = create_valid_user("John", "Doe")
+    assert user.valid?
     assert user.save, "Failed to save the user with first_name and last_name"
   end
+
+end
 end
