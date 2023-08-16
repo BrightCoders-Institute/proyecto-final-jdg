227749a7c03bbd8d3c13d834138ccaff3f2a188b
ballo1988 <rounderjards@gmail.com>
2023-08-15 18:10:05 -0600

    Now customers can see products' index but can't edit nor add new ones
    co-authored-by: hugocruz1234 cruzhugo1997@gmail.com

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :admin?, only: %i[index edit update destroy create new]
+  before_action :admin?, only: %i[ edit update destroy create new]
   before_action :set_product, only: %i[ show edit update destroy ]
 
   # GET /products or /products.json
@@ -59,13 +59,14 @@ class ProductsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @product = Product.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def product_params
-      params.require(:product).permit(:name, :description, :product_type, :brand, :image, :size, :base_price, :discount, :total_price, :stock, :availability)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def product_params
+    params.require(:product).permit(:name, :description, :product_type, :brand, :image, :size, :base_price, :discount, :total_price, :stock, :availability)
+  end
 end
