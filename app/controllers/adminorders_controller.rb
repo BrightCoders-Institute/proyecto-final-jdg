449280e01eb6295ec6f0ce807b18e89e034c6e98
ballo1988 <rounderjards@gmail.com>
2023-08-24 20:08:03 -0600

    Added Edit to status, work still in progress
    co-authored-by: hugocruz1234 cruzhugo1997@gmail.com

--- app/controllers/adminorders_controller.rb
+++ app/controllers/adminorders_controller.rb
@@ -1,11 +1,29 @@
 class AdminordersController < ApplicationController
-
+  before_action :find_order, only: %i[show edit update]
   def index
     @orders = Order.all
   end
 
-  def show
-    @order = Order.find(params[:id])
+  def show; end
+
+  def edit; end
+
+  def update
+    if @order.update(order_params)
+      redirect_to adminorders_path
+    else
+      redirect_to adminorders_path
+      # render :edit, status: :unprocessable_entity
+    end
   end
 
+  private
+
+  def order_params
+    params.require(:order).permit(:status)
+  end
+
+  def find_order
+    @order = Order.find(params[:id])
+  end
 end
