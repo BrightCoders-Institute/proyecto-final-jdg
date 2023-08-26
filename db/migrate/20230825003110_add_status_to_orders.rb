2171c86ca37427dda247328a54e153f76dd06558
ballo1988 <rounderjards@gmail.com>
2023-08-25 18:10:55 -0600

    Fixed an error that caused orders not generating with pending by default
    co-authored-by: hugocruz1234 cruzhugo1997@gmail.com

--- db/migrate/20230825003110_add_status_to_orders.rb
+++ db/migrate/20230825003110_add_status_to_orders.rb
@@ -1,5 +1,5 @@
 class AddStatusToOrders < ActiveRecord::Migration[7.0]
   def change
-    add_column :orders, :status, :string, default: "pending"
+    add_column :orders, :status, :string
   end
 end
