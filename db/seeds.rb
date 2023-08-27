178b77fee7a7c2c0eede3ab134cd0f0b47217636
Hector Rodriguez <hector.m.rm@hotmail.com>
2023-08-27 09:10:00 -0600

    add admin users

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,5 +14,21 @@ User.create!(
   last_name: "User",
   usertype: "admin"
   )
-  
+
+User.create!(
+  email: "hector.ninds@gmail.com",
+  password: "123456",
+  first_name: "Admin",
+  last_name: "User",
+  usertype: "admin"
+  )
+
+User.create!(
+  email: "jlopez114@ucol.mx",
+  password: "123456",
+  first_name: "Admin",
+  last_name: "User",
+  usertype: "admin"
+  )
+
   puts "Usuario administrador creado"
