085bf41f7e7c02f113bbfbcf5ad07b15c5ba5cc2
hugocruz1234 <cruzhugo1997@gmail.com>
2023-08-30 16:14:14 -0600

    solved some problems on the form of products  coauthor Ballo1988 rounderjards@gmail.com

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   has_many_attached :images
   belongs_to :brand
-  validates :name, :product_type, :images, :base_price, :total_price, :stock, presence: true
+  validates :name, :product_type, :base_price, :total_price, :stock, presence: true
+  validates :images, presence: true, on: :create # Requiere imágenes al crear el producto
 end
