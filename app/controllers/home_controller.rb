efd11b9f13dbf255bad71d24bf3575c530971d57
Hector Rodriguez <hector.m.rm@hotmail.com>
2023-09-14 19:17:18 -0600

    Refactored logic for loading Caps products by brand in the HomeController

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,10 @@ class HomeController < ApplicationController
     @products = Product.all
     @brands = Brand.all
     @caps_products = Product.where(product_type: 'Caps').includes(:brand)
+
+    @caps_products_by_brand = {}
+    @brands.each do |brand|
+      @caps_products_by_brand[brand.id] = @caps_products.select { |product| product.brand == brand }
+    end
   end
 end
