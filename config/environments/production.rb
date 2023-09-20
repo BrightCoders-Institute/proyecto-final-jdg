0e8b40297acd5291157818d89eb62eafb7dff68d
Juanequex <ramon.g.uz37@gmail.com>
2023-09-19 22:43:07 -0600

    Add host to mailer in prodcution

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'monkey-hats-02b5c3453387.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: ENV['USERNAME_MAILER'],
+    password: ENV['PASSWORD_MAILER'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
